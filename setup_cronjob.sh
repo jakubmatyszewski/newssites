5694e9eec6870cbd113f64f8a748a1641f876db1
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2020-11-14 18:19:49 +0100

    Add crontab reload in setup_cronjob.sh
    Fix crojob timing

--- setup_cronjob.sh
+++ setup_cronjob.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# Write user's crontab to a temporary file - crontmp
 crontab -l > crontmp
 
-echo "0/10 * * * * python3 $(pwd)/app.py >> $(pwd)/logs.txt" >> crontmp
+# Append crontmp with a new cronjob
+echo "*/10 * * * * python3 $(pwd)/app.py >> $(pwd)/logs.txt" >> crontmp
 
+# Set crontmp as a user's crontab
 crontab crontmp
+
+# Remove temporary file
 rm crontmp
+
+# Reload cron service
+/etc/init.d/cron reload
